cd866b11ba352d593e632a184fe65aa594830962
AliceMeneses <alicesilvademeneses@gmail.com>
2021-03-25 21:32:17 -0300

    Adicionar instrução para desconectar do banco quando o programa encerrar

--- src/main/java/br/com/ontimedelivery/Main.java
+++ src/main/java/br/com/ontimedelivery/Main.java
@@ -2,7 +2,10 @@ package br.com.ontimedelivery;
 
 import java.io.IOException;
 
+import br.com.ontimedelivery.dao.ConexaoBanco;
 import javafx.application.Application;
+import javafx.application.Platform;
+import javafx.event.EventHandler;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
@@ -16,13 +19,19 @@ private static Scene scene;
 	@Override
 	public void start(Stage stage) throws IOException {
 		
+		stage.setOnCloseRequest(event -> ConexaoBanco.desconectar());
+		
         Parent root = FXMLLoader.load(getClass().getResource("/view/Login.fxml"));
         
         scene = new Scene(root);
         stage.setScene(scene);
         stage.show();
+        
+
 		
 	}
+	
+	
 	
 	public static void main(String[] args) {
 		launch(args);
