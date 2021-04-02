52b20002c9000b2399dbe0e1c247daf5a14cd43d
AliceMeneses <alicesilvademeneses@gmail.com>
2021-04-02 11:26:31 -0300

    Adicionar navegação de telas

--- src/main/java/br/com/ontimedelivery/Main.java
+++ src/main/java/br/com/ontimedelivery/Main.java
@@ -11,25 +11,39 @@ import javafx.stage.Stage;
 
 public class Main extends Application{
 
-private static Scene scene;
-
+	private static Scene loginScene;
+	private static Scene pedidoScene;
+	
+	private static Stage stage;
 	
 	@Override
-	public void start(Stage stage) throws IOException {
+	public void start(Stage primariaStage) throws IOException {
+		stage = primariaStage;
 		
 		stage.setOnCloseRequest(event -> ConexaoBanco.desconectar());
 		
-        Parent root = FXMLLoader.load(getClass().getResource("/view/Login.fxml"));
+        Parent fxmlLogin = FXMLLoader.load(getClass().getResource("/view/Login.fxml"));        
+        loginScene = new Scene(fxmlLogin);
         
-        scene = new Scene(root);
-        stage.setScene(scene);
+        Parent fxmlTeste = FXMLLoader.load(getClass().getResource("/view/Pedido.fxml"));
+        pedidoScene = new Scene(fxmlTeste);
+        
+        stage.setScene(loginScene);
         stage.show();
-        
-
 		
 	}
 	
-	
+	public static void trocarScene(String scene) {
+		
+		switch (scene) {
+			case "Login":
+				stage.setScene(loginScene);
+				break;
+			case "Pedido":
+				stage.setScene(pedidoScene);
+				break;
+		}
+	}
 	
 	public static void main(String[] args) {
 		launch(args);
