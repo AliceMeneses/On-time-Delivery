583c83b2b645e467af85735d92ad7d7da6e087f7
AliceMeneses <alicesilvademeneses@gmail.com>
2021-05-16 14:39:21 -0300

    Adicionar o meio de transporte e o idioma na url, trocar o retorno para String devido futuramente o uso de um BigDecimal para calcular o frete usando a distancia e alterar a String recebida na resposta para somente o valor da distância

--- src/main/java/br/com/ontimedelivery/api/BuscaDistancia.java
+++ src/main/java/br/com/ontimedelivery/api/BuscaDistancia.java
@@ -14,15 +14,18 @@ import com.google.gson.JsonObject;
 
 public class BuscaDistancia {
 
-	public Double medirDistancia(String enderecoOrigem, String enderecoDestino) {
+	public String medirDistancia(String enderecoOrigem, String enderecoDestino) {
 
-		String urlDistanceMatrix = "https://maps.googleapis.com/maps/api/distancematrix/json?" + "origins="
-				+ enderecoOrigem + "&destinations=" + enderecoDestino + "&key=AIzaSyAXW4JQPiNoi0uuCCBebbC8peIGl8kXMTs";
+		String urlDistanceMatrix = "https://maps.googleapis.com/maps/api/distancematrix/json?"
+					+ "origins=" + enderecoOrigem 
+					+ "&destinations=" + enderecoDestino 
+					+"&mode=driving&language=pt-BR"
+					+ "&key=AIzaSyAXW4JQPiNoi0uuCCBebbC8peIGl8kXMTs";
 
 		try {
 			URL url = new URL(urlDistanceMatrix);
 			HttpURLConnection conexao = (HttpURLConnection) url.openConnection();
-
+			
 			if (conexao.getResponseCode() == 200) {
 				InputStream in = new BufferedInputStream(conexao.getInputStream());
 
@@ -31,10 +34,8 @@ public class BuscaDistancia {
 
 				String distancia = jsonObject.getAsJsonArray("rows").get(0).getAsJsonObject().getAsJsonArray("elements")
 						.get(0).getAsJsonObject().get("distance").getAsJsonObject().get("text").getAsString();
-				
-				distancia = distancia.replace("km", "");
-				
-				return Double.valueOf(distancia);
+
+				return distancia.replace("km", "").replace(",", ".").trim();
 			}
 			
 			return null;
