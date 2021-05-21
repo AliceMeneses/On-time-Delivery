4485f4e0e4dad6daadda8480368c1ddc7ad7c4fb
AliceMeneses <alicesilvademeneses@gmail.com>
2021-05-20 21:38:58 -0300

    Adicionar scene para MensagemErro.fxml, referência para o MensagemErroController, método mostrarMensagemErro() e método fecharMensagemErro()

--- src/main/java/br/com/ontimedelivery/MainFX.java
+++ src/main/java/br/com/ontimedelivery/MainFX.java
@@ -2,6 +2,7 @@ package br.com.ontimedelivery;
 
 import java.io.IOException;
 
+import br.com.ontimedelivery.controller.MensagemErroController;
 import br.com.ontimedelivery.model.Usuario;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
@@ -15,6 +16,10 @@ public class MainFX extends Application{
 	private static Scene pedidoScene;
 	private static Scene signUpScene;
 	private static Scene sucessoPedidoScene;
+	private static Scene mensagemErroScene;
+	private static Stage stageMensagemErro;
+	
+	private static MensagemErroController mensagemErroController;
 	
 	private static Stage stage;
 	
@@ -33,6 +38,12 @@ public class MainFX extends Application{
         
         Parent fxmlSucessoPedido = FXMLLoader.load(getClass().getResource("/view/SucessoPedido.fxml"));       
         sucessoPedidoScene = new Scene(fxmlSucessoPedido);
+        
+        FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("/view/MensagemErro.fxml"));
+        Parent fxmlMensagemErro = fxmlLoader.load();
+        mensagemErroController = fxmlLoader.<MensagemErroController>getController();
+        
+        mensagemErroScene = new Scene(fxmlMensagemErro);
         
         stage.setScene(loginScene);
         stage.show();
@@ -57,6 +68,20 @@ public class MainFX extends Application{
 	public static void mudarParaSucessoPedidoScene() {
 		
 		stage.setScene(sucessoPedidoScene);
+	}
+	
+	public static void mostrarMensagemErro(String mensagemErro) {
+		
+		stageMensagemErro = new Stage();
+		
+		mensagemErroController.setMensagemErro(mensagemErro);
+		stageMensagemErro.setScene(mensagemErroScene);		
+		stageMensagemErro.show();
+	}
+	
+	public static void fecharMensagemErro() {
+		
+		stageMensagemErro.close();
 	}
 	
 	public static void main(String[] args) {
